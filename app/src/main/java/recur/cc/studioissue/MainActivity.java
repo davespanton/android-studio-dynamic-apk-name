ab931025719121f6eee98db413828c59710c485a
Dave Spanton <d_spanton@hotmail.com>
2015-02-25 21:47:06 +0000

    Initial Commit.
    Basic test case application to reproduce issue with Android Studio and
    dynamic apk version names.

---
+++ app/src/main/java/recur/cc/studioissue/MainActivity.java
@@ -0,0 +1,38 @@
+package recur.cc.studioissue;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView versionNameTextView = (TextView) findViewById(R.id.version_name);
+        versionNameTextView.setText(BuildConfig.VERSION_NAME);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
